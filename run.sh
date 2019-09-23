81386d8f18004c50d047dc7f79b1b1819bd821c6
Clemens Lutz <lutzcle@cml.li>
2019-09-23 20:15:52 +0200

    Add C++11 flag

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-nvcc -O3 -lnuma -gencode arch=compute_70,code=sm_70 -o um_reprex um_reprex.cu
+nvcc -std=c++11 -O3 -lnuma -gencode arch=compute_70,code=sm_70 -o um_reprex um_reprex.cu
 ./um_reprex
